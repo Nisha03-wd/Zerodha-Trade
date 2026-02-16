94adefb311f2bc7d7b24de2a3640ddfcd595c4d6
Nisha03-wd <nisha03wd@gmail.com>
2026-02-17 00:53:31 +0530

    final backend

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -5,7 +5,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allPositions")
+    axios.get("https://zerodha-trade.onrender.com/allPositions")
       .then((res) => {
         setAllPositions(res.data);
       })
